6e67b03bbe832bbc6222a8f5ad0263353e339baa
vansh-tyagi01 <tyagiaruntyagi9@gmail.com>
2026-08-10 17:14:28 +0530

    Fix app.py

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ car=pd.read_csv("Cleaned_Car.csv")
 
 model = pickle.load(open("LinearRegressionModel.pkl","rb"))
 
-# @app.route('/')
-# def index():
-#     companies = sorted(car['company'].unique())
-#     car_models = sorted(car['name'].unique())
-#     year = sorted(car['year'].unique(),reverse=True)
-#     fuel_type = car['fuel_type'].unique()
-
-#     return render_template('index.html', companies=companies, car_models=car_models, years=year, fuel_type=fuel_type)
-
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
 
